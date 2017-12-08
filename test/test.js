d9da3e2139c902f74fc255ee8dd1b9b6bbbf307a
Tomasz jakub Rup <tomasz.rup@gmail.com>
2017-12-08 10:39:38 +0100

    added tests

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const MemcachedClient = require('../index')
+const assert = require('chai').assert
+
+const mcc = new MemcachedClient('--SERVER=127.0.0.1')
+
+describe('MemcachedClient has function', () => {
+  describe('#get', () => {
+    it('should return Buffer object', () => {
+      assert.instanceOf(mcc.get('test'), Buffer)
+    })
+    it('should return stored value', () => {
+      assert.equal(mcc.get('test').compare(Buffer.from('test')), 0)
+    })
+  })
+})
