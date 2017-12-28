150a7a629499c54003b77f4c534a88fd7cdcd5ef
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-12-28 18:36:52 +0100

    Simplify `get` function

--- src/get.cc
+++ src/get.cc
@@ -21,20 +21,16 @@ NAN_METHOD(MemcachedClient::Get)
     }
 
     std::string key = NANX_V8VALUE_TO_STRING(info[0]);
-    std::vector<char> ret_val;
     uint32_t flags = 0;
     memcached_return_t rc;
     size_t value_length = 0;
 
     char *value = memcached_get(mcc->mcc, key.c_str(), key.length(),
                                 &value_length, &flags, &rc);
-    if (value != NULL && ret_val.empty())
+    if (value != NULL)
     {
-        ret_val.reserve(value_length + 1); // Always provide null
-        ret_val.assign(value, value + value_length + 1);
-        ret_val.resize(value_length);
+        std::string ret_str(value);
         free(value);
-        std::string ret_str(ret_val.begin(), ret_val.end());
         info.GetReturnValue().Set(NANX_STRING_TO_V8BUFFER(ret_str));
     }
 }
